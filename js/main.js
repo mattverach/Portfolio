2ed2bed6c113f494b1013b087969aa8ee99453b5
Matías Vera <matias_vera@outlook.com>
2022-01-20 07:43:37 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+let $btns = $('.project-area .button-group button');
+
+$btns.click(function(e){
+
+    $('.project-area .button-group button').removeClass('active');
+    e.target.classList.add('active');
+
+    let selector = $(e.target).attr('data-filter');
+    $('.project-area .grid').isotope({
+        filter : selector
+    })
+
+    return false
+})
+
+    $('.project-area .button-group #btn1').trigger('click');
+
+});
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("home");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+} 
